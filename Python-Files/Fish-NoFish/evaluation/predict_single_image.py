6d08b64430b52b359915bd78ee2c43603f9f290b
jcgeo9 <73170251+jcgeo9@users.noreply.github.com>
2021-08-07 23:47:11 +0100

    Update predict_single_image.py

--- Python-Files/Fish-NoFish/evaluation/predict_single_image.py
+++ Python-Files/Fish-NoFish/evaluation/predict_single_image.py
@@ -7,7 +7,6 @@
 # How to use  : Replace variables in CAPS according to needs of the test set
 # =============================================================================
 
-
 import numpy as np
 import os
 import cv2
@@ -20,21 +19,23 @@ np.set_printoptions(suppress=True)
 
 path_to_image='PATH TO IMAGE'
 model_path='PATH TO SAVED MODEL'
-IMG_SIZE = 150 
+IMG_SIZE = 150
 
 #function to prepare the image in order to be given to the model
 def prepare(filepath):
-    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)  # read in the image, convert to grayscale
-    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize image to match model's expected sizing
+    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
     norm_array=new_array/255.0
-    return norm_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)  # return the image with shaping that TF wants.
+    # return the image with shaping that TF wants.
+    return norm_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
 
 #loads the saved model from the path specified
-model=tf.keras.models.load_model(model_path)
+model=tf.keras.models.load_model(model_path+"Binary_Filters_32,32,64,64,64-Dense_64_BEST")
 
 #to display the raw image
 image = Image.open(path_to_image)
-image.show() #.show() in python
+image.show()
 
 #prepares image via function call and supplies it to the model
 prediction = model.predict([prepare(path_to_image)])  #passing the list of the image
