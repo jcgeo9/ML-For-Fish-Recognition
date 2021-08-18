33cee7f247958d0151af366ae2f098b20eb9b82e
jcgeo9 <73170251+jcgeo9@users.noreply.github.com>
2021-08-18 23:08:59 +0100

    Update convert_to_tflite.py

--- Python-Files/model_conversion/convert_to_tflite.py
+++ Python-Files/model_conversion/convert_to_tflite.py
@@ -1,1 +1,21 @@
+# =============================================================================
+# Created By  : Giannis Kostas Georgiou
+# Project     : Machine Learning for Fish Recognition (Individual Project)
+# =============================================================================
+# Description : File in order to convert saved models to .tflite instances.
+#               To be used after the desired model are trained and saved
+# How to use  : Replace variables in CAPS according to needs of the dataset   
+# =============================================================================
 
+import tensorflow as tf
+
+model_path='PATH TO SAVED MODEL'
+tflite_model_name='NAME OF THE NEWLY CREATED TFLITE MODEL'
+
+#convert the model by loading the saved model to the converter
+converter = tf.lite.TFLiteConverter.from_saved_model(model_path)
+tflite_model = converter.convert()
+
+#save the tflite model
+with open(tflite_model_name+'.tflite', 'wb') as f:
+  f.write(tflite_model)
